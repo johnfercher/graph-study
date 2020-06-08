6f85704e01632f20a2de99e103d2051eb2a4be4d
jfercher <johnathan.fercher@mercadolivre.com>
2020-06-08 19:20:16 -0300

    Update volume

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 3307:3306 -e MYSQL_ROOT_PASSWORD=supersecret -e MYSQL_DATABASE=Graph -e MYSQL_USER=AdminUser -e MYSQL_PASSWORD=AdminPassword graph-db
+docker run --volume=$HOME/mysql/data:/data -d -p 3307:3306 -e MYSQL_ROOT_PASSWORD=supersecret -e MYSQL_DATABASE=Graph -e MYSQL_USER=AdminUser -e MYSQL_PASSWORD=AdminPassword graph-db
